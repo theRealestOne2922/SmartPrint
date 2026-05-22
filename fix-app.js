21c0dcc39a2207c1db56b01f2632240253774dc2
Kishore <kishoregokul808@gmail.com>
2026-05-22 20:18:09 +0530

    chore: remove temporary test and diagnosis scripts

--- fix-app.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = 'D:/SmartPrint_final/student web app/client/src/App.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/import BatchStatus from "\.\/pages\/batch-status";/g, '');
-content = content.replace(/<Route path="\/batch-status" component={BatchStatus} \/>/g, '');
-fs.writeFileSync(file, content);
-console.log('Fixed App.tsx');
--- student web app/test-db.js
+++
@@ -1,8 +0,0 @@
-import pg from 'pg';
-const { Client } = pg;
-const client = new Client({
-  connectionString: 'postgresql://postgres:bdHhJCfMXL05ekTT@db.mqqluwvemcuokqcchnii.supabase.co:5432/postgres'
-});
-client.connect()
-  .then(() => { console.log('Connected!'); client.end(); })
-  .catch(err => { console.error('Connection error:', err.message); client.end(); });
